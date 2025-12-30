9dc513ebb1e0920f0e3adee18a0c50960cd47d23
Ahmed <220901525@stu.istinye.edu.tr>
2025-12-30 22:12:41 +0300

    Intentional bug to test stop-the-line

--- app/features.py
+++ app/features.py
@@ -1,5 +1,2 @@
-import hashlib
-
-
-def hash_feature(value: str, num_buckets: int = 100) -> int:
+def hash_feature(value: str, num_buckets: int = 100)
     return int(hashlib.md5(value.encode()).hexdigest(), 16) % num_buckets
